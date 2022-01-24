c6754d29a06eb3be1d4ddcf92335d575e7529ccb
Ramji kumar <90134895+Ramjikumarcom@users.noreply.github.com>
2022-01-23 19:12:21 -0800

    react
    problem on react

---
+++ app.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+class App extends Component {
+    render() {
+        return React.createElement("h1", null, "hello geeky shows");
+    }
+}
+export default App;
